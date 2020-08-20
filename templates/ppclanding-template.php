39e5c049cdfbbf4c59a4bc2c4a53594556eabc6a
Zachary Sanders <zacharysanders@Zacharys-MacBook-Pro.local>
2020-08-20 15:42:08 -0600

    Adds pagetemplater class and template

---
+++ templates/ppclanding-template.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * C1 Partners PPC Landing Page Template
+ *
+ * @link https://c1partners.com
+ *
+ */
+
+
+get_header();
+
+while ( have_posts() ) :
+
+  the_post();
+
+  ?>
+  
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="page-header">
+      <!-- <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?> -->
+    </header>
+    <div class="page-content">
+      <?php the_content(); ?>
+    </div>
+  </article>
+
+  <?php
+endwhile;
+
+get_footer();
